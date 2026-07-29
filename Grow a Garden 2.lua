7681743265f96fa693af53687cea8f00ba3067be
ItzYumi <ddumacc001@gmail.com>
2026-07-29 17:17:18 +0800

    Update Grow a Garden 2.lua

--- Grow a Garden 2.lua
+++ Grow a Garden 2.lua
@@ -1,4 +1,4 @@
 -- This file was protected using Luraph Obfuscator v14.7 [https://lura.ph/]
 
 loadstring(game:HttpGet("https://gist.githubusercontent.com/ItzYumi/bfc8d2cba9ce6a7b1fe12ddc687b4bc8/raw/3e7e23120cde1e6390d7873071a7ca914a2ccf9f/UI.luau"))()
-loadstring(game:HttpGet("https://gist.githubusercontent.com/ItzYumi/e8530d9a3d088d61f4c125cc3e8685d8/raw/11eab440c1a3a675ca1e99f31523a2e39bd40294/Function.luau"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/ItzYumi/171a426a534f6af18234ead6cb7e9af0/raw/d10c9c6f5ba2b5114c805870b6a59f5b0f4f0c20/Function.luau"))()
